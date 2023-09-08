869c8c3ad3835c4f4d4c61378b5f2ba3d98732f7
sri1217 <srisandy2002@gmail.com>
2023-09-08 08:23:13 +0530

    Added-file

---
+++ demo.js
@@ -0,0 +1,7 @@
+function sri2(){
+    alert("hii bro how are you")
+    document.write("Welcome to java World")
+}
+function dri3(){
+    document.getElementById("myimage").src="https://www.w3schools.com/js/pic_bulboff.gif"
+}
