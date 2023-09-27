742244c4d4c598924898893f2e57d7b8e2ba3b0e
SriHari1217 <srisandy2002@gmail.com>
2023-09-27 08:53:21 +0530

    Added-file

---
+++ Course/Project/Quanti.js
@@ -0,0 +1,7 @@
+let text = "hi bro hior are you hioe u doing werhi zelo  zzzzzzznnzzzzzzzz hhhhhhhhhhhhh"
+let b = text.match(/z+/g);
+let c = text.match(/z*/g);
+let d = text.match(/z?/g);
+console.log(b);
+console.log(c);
+console.log(d);
--- Course/Project/regex.js
+++ Course/Project/regex.js
@@ -8,14 +8,23 @@ let out4 = txt.match(pattern);
 let data = "Come to the java and to the java script with come to java"
 let asd =data.replaceAll(/JAVA/ig,"C");
 let qwert = asd.replaceAll(/C/ig,"sri")
-// console.log(out);
-// console.log(out1);
-// console.log(out2);
-// console.log(out3);
-// console.log(out4);
-// console.log(asd);
-// console.log(qwert);
+console.log(out);
+console.log(out1);
+console.log(out2);
+console.log(out3);
+console.log(out4);
+console.log(asd);
+console.log(qwert);
 let zxcv = "hi hello to the hi to the hello and hi for hello"
 let pattern1=(/(HELLO|HI)/ig);
 let out5 = zxcv.match(pattern1);
 console.log(out5);
+const txt1 = "ji slksdnsf lskfafsdlkej kasjesl";
+let par =/s/g;
+let c =par.exec(txt1);
+
+console.log(c);
+
+
+
+
