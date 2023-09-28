b3366d12cf53559ffb5c7363a78b73c5fa41bad8
SriHari1217 <srisandy2002@gmail.com>
2023-09-28 10:06:38 +0530

    Added-File

---
+++ Course/Project/Class.js
@@ -0,0 +1,10 @@
+class Employee{
+    constructor(id,name) {
+        this.id =id;
+        this.name =name;
+        console.log(this.id+" "+this.name)
+    }
+    
+}
+ var msg=new Employee("1003","sri");
+ var msg1=new Employee("1002","hari");
