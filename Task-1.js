3b9a9eb34c4fd7e2a87bc727cb2f3004475664f9
SriHari1217 <srisandy2002@gmail.com>
2023-09-30 09:07:00 +0530

    Added-File

--- Task-1.js
+++ Task-1.js
@@ -1,6 +1,48 @@
 function task1(){
-    var a= "Coimbatore"
-    var b= "Doctor"
-    console.log("You will be a" + " " + b + " "+ a);
+
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+
+function logDairy() {
+  for (const product of dairy) {
+    console.log(product);
+  }
+}
+
+logDairy();
+
+
+const animal = {
+  canJump: true
+};
+
+const bird = Object.create(animal);
+bird.canFly = true;
+bird.hasFeathers = true;
+
+function birdCan() {
+  for (const key in bird) {
+    if (bird.hasOwnProperty(key)) {
+      console.log(`${key}: ${bird[key]}`);
+    }
+  }
+}
+
+birdCan();
+
+
+function animalCan() {
+//   for (const key in bird) {
+
+//     console.log(`${key}: ${bird[key]}`);
+//     break;
+
+//   }
+  for (const key in animal) {
+    console.log(`${key}: ${animal[key]}`);
+  }
+}
+
+animalCan();
+
 }
 task1()
